a52519ca149ec4b39ab111a9167e13a255a531e4
stud90558 <Dudinskaasofia@gmail.com>
2025-10-28 19:35:41 +0300

    Add files via upload

---
+++ Лабораторная ЯП(4)/Задание 2.2.cpp
@@ -0,0 +1,51 @@
+﻿// Задание 2.2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int suml(vector<int>& array) {
+    int lastind = -1;
+    for (int i = 0; i < array.size(); i++) {
+        if (array[i] > 0) {
+            lastind = i;
+        }
+    }
+
+    if (lastind == -1) {
+        return 0;
+    }
+
+    int sum = 0;
+    for (int i = 0; i < lastind; i++) {
+        sum += array[i];
+    }
+    return sum;
+}
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    int n;
+    cout << "Введите размер массива: ";
+    cin >> n;
+
+    vector <int> array(n);
+    cout << "Введите элементы массива:\n";
+    for (int i = 0; i < n; i++) {
+        cin >> array[i];
+    }
+
+    int result = suml(array);
+
+    if (result == 0) {
+        cout << "В массиве нет положительных элементов или сумма равна нулю." << endl;
+    }
+    else {
+        cout << "Сумма элементов до последнего положительного элемента: " << result << endl;
+    }
+
+    return 0;
+}
+
+
---
+++ Лабораторная ЯП(4)/Задание 3.2.cpp
@@ -0,0 +1,41 @@
+﻿// Задание 3.2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <vector>
+#include <cmath>
+using namespace std;
+
+void sm(int array[], int size) {
+    int h = 0; 
+
+    for (int i = 0; i < size; i++) {
+        int mod = abs(array[i]);
+        if (mod < 5 || mod > 10) {
+            array[h++] = array[i];
+        }
+    }
+    for (;h < size; h++) {
+        array[h] = 0;
+    }
+}
+
+
+    int main()
+    {
+
+        setlocale(LC_ALL, "ru");
+        int array[] = { 1, 5, -1, -10, -2, 3, 6.3 };
+        int size = sizeof(array) / sizeof(array[0]);
+        cout << "Исходный массив: " << endl;
+
+        for (int i = 0; i < size; i++)
+            cout << array[i] << " " << endl;
+
+        sm(array, size);
+        cout << "Сжатый массив: ";
+        for (int i = 0; i < size; i++)
+            cout << array[i] << " " << endl;
+
+        return 0;
+    }
