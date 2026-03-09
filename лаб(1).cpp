526622e3c0dd071bf134c3f7d536cb6c3ce75fb2
stud90558 <Dudinskaasofia@gmail.com>
2026-03-09 20:01:26 +0300

    Add files via upload

---
+++ лаб(1).cpp
@@ -0,0 +1,47 @@
+﻿#include "Film.h"
+#include <iostream>
+#include <vector>
+#include <limits>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "ru_RU.UTF-8");
+
+    string filename;
+    cout << "Введите имя файла с данными о фильмах : ";
+    getline(cin, filename);
+    if (filename.empty()) {
+        filename = "films.txt";
+    }
+
+    vector<Film> films;
+
+    // Защита от дурака
+    if (!readFilmsFromFile(filename, films)) {
+       cerr << "Программа завершена." << endl;
+        return 1;
+    }
+
+    // Контрольный вывод считанных данных
+    cout << " Исходный список фильмов ";
+    printFilms(films);
+
+    if (films.empty()) {
+        cout << "Нет записей для сортировки.";
+        return 0;
+    }
+
+    // Сортировка по году 
+    vector<Film> filmsByYear = films;
+    insertionSortByYearDesc(filmsByYear);
+    cout << " После сортировки по ГОДУ ";
+    printFilms(filmsByYear);
+
+    // Сортировка по названию 
+    vector<Film> filmsByTitle = films;
+    insertionSortByTitleAsc(filmsByTitle);
+    cout << " После сортировки по НАЗВАНИЮ ";
+    printFilms(filmsByTitle);
+
+    return 0;
+}
